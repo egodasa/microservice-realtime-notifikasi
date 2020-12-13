7671845166de274c6dc3f175b1a69019667ab213
egodasa <egodasa@gmail.com>
2020-12-13 20:49:59 +0700

    Penyelesaian

--- index.js
+++ index.js
@@ -1,60 +1,118 @@
+// PENGATURAN APLIKASI
+var config = {
+  private_key_jwt: "pakakeh",
+  allowed_host: "*",
+  allowed_methods: ["GET", "POST"],
+  jwt_algorithm: ['HS256']
+}
+
+var jwt = require('express-jwt');
 var app = require('express')();
 var http = require('http').createServer(app);
-var io = require('socket.io')(http, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"]
-  }
-});
-
-var sc
-var cors = require('cors')
-
-app.use(cors())
-
 var bodyParser = require('body-parser')
+
 app.use(bodyParser.json());       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 })); 
 
-app.all('/', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+// KONFIGURASI CORS
+app.use(function(req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "X-Requested-With");
+  res.setHeader('Content-Type', 'application/json');
   next();
- });
+});
 
-app.get('/emit', (req, res) => {
-  var channel = req.query.channel || "default";
-  var message = req.query.message || "default";
+// KONFIGURASI SOCKET
+var io = require('socket.io')(http, {
+  cors: {
+    origin: config.allowed_host, 
+    methods: config.allowed_methods
+  }
+});
+
+
+
+// ROUTE UNTUK EMIT EVENT KE CLIENT
+app.post('/emit', jwt({
+  secret: config.private_key_jwt,
+  algorithms: config.jwt_algorithm
+}), (req, res) => {
+  if (!req.user) {
+    res.status(401).json({
+      status: false,
+      code: 401,
+      message: "Token tidak valid"
+    });
+  }
+  
+  var err = {
+    channel: "",
+    message: ""
+  }
+
+  if(!req.body.channel)
+  {
+    err.channel = "Nama channel tidak boleh kosong"
+  }
+
+  if(!req.body.message)
+  {
+    err.message = "Pesan tidak boleh kosong"
+  }
+
+  if(!req.body.message || !req.body.channel)
+  {
+    res.status(422).json({
+      status: false,
+      code: 422,
+      message: "Data tidak valid",
+      data: err
+    });
+  }
+
+  var channel = req.body.channel;
+  var message = req.body.message;
+
+  // EMIT EVENT KE CLIENT
   io.emit(channel, message);
+
   console.log("Nama channel: " + channel);
   console.log("Nama message: " + message);
-  res.send("ok");
+
+  res.json({
+    status: true,
+    code: 200,
+    message: "OK"
+  });
 })
+
+// KONFIGURASI RESPON JWT ERROR 
+app.use(function (err, req, res, next) {
+  if (err.name === 'UnauthorizedError') {
+      res.status(401).send(JSON.stringify({
+        status: false,
+        code: 401,
+        message: "Token tidak valid"
+      }));
+  }
+});
 
 // server membuat koneksi socket
 io.on('connection', (socket) => {
-    // sc = socket
-
-    // sc.on('channel_1', function(msg){
-
-    //   console.log(sc.tambahan)
-    //   // setelah listem server akan emit ke klien lagi dengan kode dibawah
-    //   io.emit('channel_1', msg);
-
-    //   // room agar notifikasi bisa disebar ke user tertentu saja
-    //   socket.join('channel_1:' + socket.handshake.query.nama_room);
-    // });
-  
-    // sc.on('channel_2', function(msg){
-    //   console.log(sc.tambahan)
-    //   // setelah listem server akan emit ke klien lagi dengan kode dibawah
-    //   io.emit('channel_2', msg);
-    // });
 
 });
 
+
+// HANDLE 404 NOT FOUND
+app.use(function(req,res){
+  res.status(404).json({
+    status: false,
+    message: "Not found",
+    code: 404
+  });
+});
 
 http.listen(3000, () => {
   console.log('listening on *:3000');
---
+++ readme.md
@@ -0,0 +1,13 @@
+# Microservice Websocket With NodeJS
+Dengan microservice ini, aplikasi Anda bisa mendapatkan fitur realtie notification.
+Caranya adalah aplikasi utama Anda hanya perlu mengakses API microservice ini dan otomatis aplikasi Anda akan menerima notifikasi websocket
+
+Cara pakai :
+1. Clone repo ini
+1. Buka file index.js dan baca bagian konfigurasi (baris 1 - 10) (OPTIONAL)
+1. Install dependency `npm install`
+1. Jalankan aplikasi ini `node index.js`
+1. Panggil API aplikasi ini dengan URL `/emit` dengan parameter : <br> channel: Nama channel yang ingin dibroadcast <br> message: pesan yang ingin dikirim lewat websocket <br> token: token JWT yang digenerate sesuai konfigurasi di `index.js`
+1. Contoh pemakaian pada klien dapat dilihat di file `index.html`
+1. Microservice ini menggunakan library `socket.io` untuk menggunakan fitur websocket. Baik sisi server maupun klien.
+1. Microservice ini dapat dikombinasikan dengan berbagai macam perangkat lunak tergantung ketersediaan `socket.io` terhadap sebuah bahasa pemrograman
